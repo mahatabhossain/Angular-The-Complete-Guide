9b18a1c79692d45b2286a655262b94e26b394075
mahatabhossain <mahatabhossain28@gmail.com>
2022-08-02 15:57:28 +0530

    Basic app created, debugging process added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,17 @@ import { FormsModule } from "@angular/forms";
 
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
-import { ServerComponent } from "./server/server/server.component";
+// import { ServerComponent } from "./server/server.component";
+// import { HeaderComponent } from "./header/header.component";
+// import { RecipesComponent } from "./recipes/recipes.component";
+// import { RecipesListComponent } from "./recipes/recipes-list/recipes-list.component";
+// import { RecipesDetailComponent } from "./recipes/recipes-detail/recipes-detail.component";
+// import { RecipeItemComponent } from "./recipes/recipes-list/recipe-item/recipe-item.component";
+// import { ShoppingListComponent } from "./shopping-list/shopping-list.component";
+// import { ShoppingEditComponent } from "./shopping-list/shopping-edit/shopping-edit.component";
 
 @NgModule({
-  declarations: [AppComponent, ServerComponent],
+  declarations: [AppComponent],
   imports: [BrowserModule, AppRoutingModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
